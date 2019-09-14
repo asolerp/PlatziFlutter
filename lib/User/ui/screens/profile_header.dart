1762cc1224f299aa15f45bba17aa3efe0092d8b6
Mac <albertosolpal@gmail.com>
2019-09-14 12:44:40 +0200

    user returned from firebase working

--- lib/User/ui/screens/profile_header.dart
+++ lib/User/ui/screens/profile_header.dart
@@ -1,39 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_app/User/model/user.dart';
 import '../widgets/user_info.dart';
 import '../widgets/button_bar.dart';
+import 'package:platzi_app/User/bloc/bloc_user.dart';
 
 class ProfileHeader extends StatelessWidget {
+
+  UserBloc userBloc;
+  User user;
+
   @override
   Widget build(BuildContext context) {
 
-    final title = Text(
-      'Profile',
-      style: TextStyle(
-          fontFamily: 'Lato',
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
-          fontSize: 30.0
-      ),
-    );
+    userBloc = BlocProvider.of<UserBloc>(context);
 
-    return Container(
-      margin: EdgeInsets.only(
-          left: 20.0,
-          right: 20.0,
-          top: 50.0
-      ),
-      child: Column(
-        children: <Widget>[
-          Row(
-            children: <Widget>[
-              title
-            ],
-          ),
-          UserInfo('assets/img/ann.jpg', 'Anahí Salgado','anahi@platzi.com'),
-          ButtonsBar()
-        ],
-      ),
+    return StreamBuilder(
+      stream: userBloc.streamFirebase,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        switch(snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return CircularProgressIndicator();
+          case ConnectionState.none:
+            return CircularProgressIndicator();
+          case ConnectionState.active:
+            return showProfileData(snapshot);
+          case ConnectionState.done:
+            return showProfileData(snapshot);
+        }
+      },
     );
+  }
+
+  Widget showProfileData(AsyncSnapshot snapshot) {
+    if(!snapshot.hasData || snapshot.hasError) {
+     print("No logeado");
+     return Container(
+       margin: EdgeInsets.only(
+           left: 20.0,
+           right: 20.0,
+           top: 50.0
+       ),
+       child: Column(
+         children: <Widget>[
+           CircularProgressIndicator(),
+           Text("No se pudo cargar la información")
+         ],
+       ),
+     );
+    } else {
+      print("Logeado");
+      print(snapshot.data);
+      user = User(name: snapshot.data.displayName, email: snapshot.data.email, imgProfile: snapshot.data.photoUrl);
+      final title = Text(
+        'Profile',
+        style: TextStyle(
+            fontFamily: 'Lato',
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+            fontSize: 30.0
+        ),
+      );
+
+      return Container(
+        margin: EdgeInsets.only(
+            left: 20.0,
+            right: 20.0,
+            top: 50.0
+        ),
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                title
+              ],
+            ),
+            UserInfo(user),
+            ButtonsBar()
+          ],
+        ),
+      );
+    }
   }
 
 }
